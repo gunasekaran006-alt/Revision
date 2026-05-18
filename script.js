140d2852691f71b98835f6c575039e89a35163f9
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-05-18 22:52:25 +0530

    updates-modulus-Exponentiation operators

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ let b = 10;
 console.log(a+b); // 10 - Addition operator
 console.log(a-b); // -10 - Subtraction operator
 console.log(a*b); // 0 -  Multipilcation operator
-console.log(b/a); // Infinity - Division operator
+console.log(b/a); // ~ Infinity - Division operator
 console.log(a/b); // 0 - Division operator
+// ***Future Topic:
+let c = 9;
+let d = 2;
+console.log(c%d); // 1 Remainder - Modulus operators
+let base = 2; // (Base)
+let power = 3; // (Exponent)
+console.log(base ** power); // 8 - Exponentiation operators
