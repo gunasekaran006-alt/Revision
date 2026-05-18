a4bd31684dba13fd60c8f1eabe92d2a039343b43
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-05-18 22:10:29 +0530

    updates-Arithmetic-operators

---
+++ script.js
@@ -0,0 +1,8 @@
+// Arithmetic operator
+let a = 0;
+let b = 10;
+console.log(a+b); // 10 - Addition operator
+console.log(a-b); // -10 - Subtraction operator
+console.log(a*b); // 0 -  Multipilcation operator
+console.log(b/a); // Infinity - Division operator
+console.log(a/b); // 0 - Division operator
