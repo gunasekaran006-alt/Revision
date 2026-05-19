996d27ca1b179df063bf56c8c71f346cea8bf899
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-05-19 22:42:05 +0530

    updates

--- script.js
+++ script.js
@@ -1,69 +1,117 @@
-// Arithmetic operator
-let a = 0;
-let b = 10;
-console.log(a+b); // 10 - Addition operator
-console.log(a-b); // -10 - Subtraction operator
-console.log(a*b); // 0 -  Multipilcation operator
-console.log(b/a); // ~ Infinity - Division operator
-console.log(a/b); // 0 - Division operator
-// ***Future Topic:
-let c = 9;
-let d = 2;
-console.log(c%d); // 1 Remainder - Modulus operators
-let base = 2; // (Base)
-let power = 3; // (Exponent)
-console.log(base ** power); // 8 - Exponentiation operators
+// Conditional Statement
 
-// Assignment operator
-const name = "gk"; // Basic Assignment
-// pre-increment | post-increment
-let num = 10;
-num += 18; // pre-increment - 28
-console.log(num);
+// if else
+type: 1
+let age = 17;
+if (age >= 18) {
+    console.log("You're eligible to vote");
+    console.log("Please collect your voter id proof");
+} else {
+    console.log("You are not eligible to vote");
+}
+type: 2
+let marks = 30;
+if (marks >= 90) {
+    console.log("Grade A");
+}
+else if (marks >= 70) {
+    console.log("Grade B");
+}
+else if (marks >= 50) {
+    console.log("Grade C");
+}
+else {
+    console.log("Fail");
+}
 
-let age = 13;
-age =+ 18; // post-increment - 18
-age = 18; // post-increment - 18 ( generally this type common used)
-console.log(age);
+// Interview Scenario (product companies interview question)
+// optimized code
+// nested conditions
+let score = 43;
+if (score >= 50) {
+    // grade checking
+    if (score >= 90) {
+        console.log("Grade A");
+    }
+    else if (score >= 75) {
+        console.log("Grade B");
+    }
+    else {
+        console.log("Grade C");
+    }
+} else {
+    console.log("Fail");
+}
+// Alternate method:(Guard Clause)
+function checkGrade(score) {
+if (score < 50) {
+    return "Fail";
+}
+if (score >=90) return "Grade A";
+if (score >=75) return "Grade B";
+return "Grade C"
+}
+console.log(checkGrade(43)); // Fail
+console.log(checkGrade(85)); // Grade B
+
+// switch case
+let day = 2; // monday
+switch(day){
+    case 1:
+        console.log("Sunday");
+        break;    
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    default:
+        console.log("Invalid Day");
+}
 
 
-// Comparison Operator
-let eligibleAge = 18; 
-let userAge = 28;
-console.log(userAge < eligibleAge ); // false - lesser than
-console.log(userAge > eligibleAge ); // true - greater than
-console.log(userAge >= eligibleAge); // true - greater than or equal
-console.log(userAge <= eligibleAge); // false - lesser than or equal
+// Looping Statement:-
+// 1) initial value
+// 2) conditions
+// 3) iteration
+// for loop : (type: 1) - Definite Loop
+for(let i=0; i < 5; i++) {
+    console.log(i); // 0 1 2 3 4 
+}
 
-// == | ===
-// == (double equal to checks the value)
-// === (triple equal to checks the value and data-type)
-let myNumber = 18; 
-let myString = "18";
-console.log(myNumber == myString); // type coercion
-console.log(myNumber === myString); // Strict Equality - mostltly preferrable - best way 
+// while loop (type: 2) - Indefinite Loop
+let j = 1; // initial value
+while(j <=5) { // condition
+    console.log(j); //  1 2 3 4 5
+    j++;  // iteration
+}
+//more example
+let playerAlive = true;
+while (playerAlive === true){
+if(score === 0){
+    playerAlive = false;
+}
+}
 
-// Ternary operator
-// ? -> true block : -> false block
-Type:1
-let loginIn = true;
-loginIn ? console.log("login successfully")
-: console.log("please login");
-Type:2
-let mark = 340;
-// mark >=345 ? console.log("pass")
-// : console.log("fail");
-let result = mark >=345 ? "Pass" : "Fail"
-console.log(result);
-Type:3
-let mail = "guna@gmail.com"
-mail === "guna@gmail.com" ? console.log("Email Register SuccessFully")
-: console.log("Check Your Email");
+// Do-While loop (type: 3)
+let k = 1;
+do{
+    console.log(k);
+    k++;
+}while(k <=10);
 
-// Expressions
-let userName = "gk";
-console.log("You're successfully logged in" + userName); // You're successfully logged ingk - avoid this method
-console.log("You're successfully logged in", userName); // You're successfully logged in gk - avoid this method
-// backtricks (``) - used this way only global standard
-// curly brackets {}
-console.log(`You're Successfully Logged in ${userName} Welcome Back `);//
+// Industry scenario
+// User Authentication - Login validation
+let username = "narasimhan";
+let password = "narasimhan@321";
+let enteredUsername = "narasimha";
+let enteredPassword = "narasimhan@321";
+// Logical Operator (AND)
+if(enteredUsername === username && enteredPassword === password){
+    console.log("Login Successful");
+} else {
+    console.log("Invalid Credentials");
+}
+
+
